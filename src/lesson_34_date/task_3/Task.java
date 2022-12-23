b7858d2a3abd0a7a640378816ae93c0bb19b94bc
And7Se-HomePC <sirius.dzr@gmail.com>
2022-12-23 20:22:35 +0300

    Refactor 32t3.

--- src/lesson_34_date/task_3/Task.java
+++ src/lesson_34_date/task_3/Task.java
@@ -16,23 +16,41 @@ import java.util.Scanner;
 
 public class Task {
     private static final Scanner SC = new Scanner(System.in);
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
 
     public static void main(String[] args) {
-        String early = getInput("early");
-        String late = getInput("late");
+//        String early = getInput("early");
+//        String late = getInput("late");
+//        SC.close();
+//
+//        System.out.println(getValuesBetween(makeDateTimeObj(early), makeDateTimeObj(late)));
+//        System.out.println(getPeriodBetween(makeDateTimeObj(early), makeDateTimeObj(late)));
 
-        System.out.println(getValuesBetween(makeDateTimeObj(early), makeDateTimeObj(late)));
+        LocalDateTime earlyDate = makeDateTimeObj("28.06.1932 02:51:56"); // mock
+        LocalDateTime lateDate = makeDateTimeObj("20.12.2022 20:16:00"); // mock
 
-        System.out.println(getPeriodBetween(makeDateTimeObj(early), makeDateTimeObj(late)));
-
-        //LocalDateTime earlyDate = makeDateTimeObj("28.06.1932 02:51:56"); // mock
-        //LocalDateTime lateDate = makeDateTimeObj("20.12.2022 20:16:00"); // mock
-
-        //System.out.println(getValuesBetween(earlyDate, lateDate));
-        //System.out.println(getPeriodBetween(earlyDate, lateDate));
+        System.out.println(getValuesBetween(earlyDate, lateDate));
+        System.out.println(getPeriodBetween(earlyDate, lateDate));
     }
 
+    private static LocalDateTime makeDateTimeObj(String input) {
+        return LocalDateTime.parse(input, FORMATTER);
+    }
+
+    private static String getInput(String msg) {
+        System.out.printf("(%s datetime) <== ", msg);
+
+        return SC.nextLine();
+    }
+
+    // first solution variant
     private static String getValuesBetween(LocalDateTime earlyDate, LocalDateTime lateDate) {
+        if (earlyDate.isAfter(lateDate)) {
+            LocalDateTime temp = earlyDate;
+            earlyDate = lateDate;
+            lateDate = temp;
+        }
+
         long years = earlyDate.until(lateDate, ChronoUnit.YEARS);
         long months = earlyDate.until(lateDate, ChronoUnit.MONTHS);
         long days = earlyDate.until(lateDate, ChronoUnit.DAYS);
@@ -40,15 +58,13 @@ public class Task {
         long minutes = earlyDate.until(lateDate, ChronoUnit.MINUTES);
         long seconds = earlyDate.until(lateDate, ChronoUnit.SECONDS);
 
-        // rewrite it more gracefully!
-        String summary = String.format(
-                "%d years, %d months, %d days, %02d hours %02d minutes %02d seconds",
+        String summary = "%d years, %d months, %d days, %02d hours %02d minutes %02d seconds".formatted(
                 years,
-                earlyDate.until(lateDate.minus(years, ChronoUnit.YEARS), ChronoUnit.MONTHS),
-                earlyDate.until(lateDate.minus(months, ChronoUnit.MONTHS), ChronoUnit.DAYS),
-                earlyDate.until(lateDate.minus(days, ChronoUnit.DAYS), ChronoUnit.HOURS),
-                earlyDate.until(lateDate.minus(hours, ChronoUnit.HOURS), ChronoUnit.MINUTES),
-                earlyDate.until(lateDate.minus(minutes, ChronoUnit.MINUTES), ChronoUnit.SECONDS)
+                earlyDate.until(lateDate.minusYears(years), ChronoUnit.MONTHS),
+                earlyDate.until(lateDate.minusMonths(months), ChronoUnit.DAYS),
+                earlyDate.until(lateDate.minusDays(days), ChronoUnit.HOURS),
+                earlyDate.until(lateDate.minusHours(hours), ChronoUnit.MINUTES),
+                earlyDate.until(lateDate.minusMinutes(minutes), ChronoUnit.SECONDS)
         );
 
         return """
@@ -62,12 +78,18 @@ public class Task {
                 """.formatted(seconds, minutes, hours, days, months, years, summary);
     }
 
+    // second solution variant
     private static String getPeriodBetween(LocalDateTime earlyDate, LocalDateTime lateDate) {
+        if (earlyDate.isAfter(lateDate)) {
+            LocalDateTime temp = earlyDate;
+            earlyDate = lateDate;
+            lateDate = temp;
+        }
+
         Period period = Period.between(earlyDate.toLocalDate(), lateDate.toLocalDate());
         Duration duration = Duration.between(earlyDate, lateDate);
 
-        String summary = String.format(
-                "%d years, %d months, %d days, %02d hours %02d minutes %02d seconds",
+        String summary = "%d years, %d months, %d days, %02d hours %02d minutes %02d seconds".formatted(
                 period.getYears(),
                 period.getMonths(),
                 period.getDays(),
@@ -84,22 +106,14 @@ public class Task {
                   Months: %d
                   Years: %d
                   Summary: %s
-                """.formatted(duration.toSeconds(),
+                """.formatted(
+                duration.toSeconds(),
                 duration.toMinutes(),
                 duration.toHours(),
                 duration.toDays(),
                 period.toTotalMonths(),
                 period.getYears(),
-                summary);
-    }
-
-    private static LocalDateTime makeDateTimeObj(String input) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
-        return LocalDateTime.parse(input, formatter);
-    }
-
-    private static String getInput(String msg) {
-        System.out.printf("(%s) <== ", msg);
-        return SC.nextLine();
+                summary
+        );
     }
 }
