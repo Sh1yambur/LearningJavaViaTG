1a5b373c2d0a4fb79445e03e7fa87b52006fcde9
And7Se-HomePC <sirius.dzr@gmail.com>
2022-12-27 16:40:21 +0300

    Add solution 30t4

--- src/lesson_30_regex/task_4/Task.java
+++ src/lesson_30_regex/task_4/Task.java
@@ -1,7 +1,84 @@
 package lesson_30_regex.task_4;
 
+/*
+ * Реализуйте программу, разбивающую исходный текст на составные части.
+ * Текст – на абзацы (разделены ‘\n’), абзацы на предложения (разделены "."/ "?"/ "?!"/ "!"/ "…").
+ * Предложения на слова (разделены пробелами, но также могут использоваться знаки препинания ","/ "-", ":").
+ * После этого выведите на экран исходный текст.
+ * Предложения допустимо разделить точками, слова – пробелами без сохранения знаков пунктуации внутри предложения.
+ * Сохранение изначальной пунктуации, на мой взгляд, излишне усложнит задачу и сдвинет акцент с практики регулярных выражений.
+ * Подумайте, как наиболее корректно декомпозировать ваше решение.
+ *
+ * В качестве совета: попробуйте разные подходы для решения задачи,
+ * нет необходимости реализовать наиболее оптимальным способом.
+ * Например, одним из вариантов упрощения исходной задачи может стать избавление от лишних символов
+ * или их замена одним определенным через метод replaceAll() класса String.
+ */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringJoiner;
+
 public class Task {
+    private static final File FILE = new File("./resource/task_30/text.txt");
+    private static final String TEXT = readText();
+
     public static void main(String[] args) {
-        // todo
+        System.out.println(getNormalizeText(TEXT));
+    }
+
+    private static String getNormalizeText(String text) {
+        StringBuilder builder = new StringBuilder();
+
+        for (String prg : getParagraphs(text)) {
+
+            for (String snt : getSentences(prg)) {
+                String[] wrd = getWords(snt);
+
+                for (int i = 0; i < wrd.length; i++) {
+                    builder.append(wrd[i]);
+
+                    if (i == wrd.length - 1) {
+                        builder.append(". ");
+                    } else {
+                        builder.append(" ");
+                    }
+                }
+
+            }
+
+        }
+
+        return builder.toString();
+    }
+
+    private static String[] getParagraphs(String str) {
+        return str.split(System.lineSeparator());
+    }
+
+    private static String[] getSentences(String str) {
+        return str.split("\n|[.]|[?]|!|[?!]|[.]{3}");
+    }
+
+    private static String[] getWords(String str) {
+        return str.trim().split("(, )|( - )|(: )");
+    }
+
+    private static String readText() {
+        StringJoiner joiner = new StringJoiner(System.lineSeparator());
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE))) {
+            String str;
+
+            while ((str = reader.readLine()) != null) {
+                joiner.add(str);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return joiner.toString();
     }
 }
